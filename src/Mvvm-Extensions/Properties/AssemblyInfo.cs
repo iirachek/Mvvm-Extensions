de6457d68969c7480682f1f6f6c907fea933d12a
Illya Irachek <illyairachek@gmail.com>
2019-05-14 23:03:31 +0300

    New nuget packaging method

--- src/Mvvm-Extensions/Properties/AssemblyInfo.cs
+++ src/Mvvm-Extensions/Properties/AssemblyInfo.cs
@@ -9,9 +9,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Mvvm Extensions Library")]
-[assembly: AssemblyDescription("Mvvm Extensions library for WPF")]
+[assembly: AssemblyDescription("The Mvvm Extensions package provides basic implementation of ICommand interface, base class implementation for ViewModels and a range of other classes that may be useful for desktop WPF application.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Illia Irachek")]
 [assembly: AssemblyProduct("Mvvm Extensions Library")]
 [assembly: AssemblyCopyright("Copyright (C) Illia Irachek. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
@@ -24,3 +24,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("31c043a4-7d7b-42ce-81da-562a2d742026")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
+
---
+++ src/Mvvm-Extensions/pack.bat
@@ -0,0 +1,1 @@
+nuget pack MvvmExtensions.csproj -Prop Configuration=Release
