5d0af76bbae79cdd14440bd9587cde5c182e78a6
Illya Irachek <illyairachek@gmail.com>
2020-06-20 18:21:40 +0300

    Added Converters usage example

---
+++ src/MvvmExtensions.Samples/ExampleViews/ConvertersExample/ConvertersExampleView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace MvvmExtensions.Samples.ExampleViews
+{
+    /// <summary>
+    /// Interaction logic for ConvertersExampleView.xaml
+    /// </summary>
+    public partial class ConvertersExampleView : UserControl
+    {
+        public ConvertersExampleView()
+        {
+            InitializeComponent();
+            DataContext = new ConvertersExampleViewModel();
+        }
+    }
+}
---
+++ src/MvvmExtensions.Samples/ExampleViews/ConvertersExample/ConvertersExampleViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MvvmExtensions.PropertyChangedMonitoring;
+
+namespace MvvmExtensions.Samples.ExampleViews
+{
+    class ConvertersExampleViewModel : PropertyChangedImplementation
+    {
+        bool _boolSwitch = true;
+        public bool BoolSwitch
+        {
+            get
+            {
+                return _boolSwitch;
+            }
+            set
+            {
+                if (_boolSwitch != value)
+                {
+                    _boolSwitch = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
